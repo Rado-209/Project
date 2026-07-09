b2fd04811e46e67f03eb4476fefee3b972dc6196
Rado-209 <radrum09@gmail.com>
2026-07-09 16:17:06 +0300

    Add Parking class

---
+++ Parking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Parking
+    {
+        public string ParkingID { get; set; }
+        public string Location { get; set; }
+        public int TotalSpaces { get; set; }
+        public int AvailableSpaces { get; set; }
+        public List<string> Vehicles { get; set; }
+
+        public Parking(string id, string location, int totalSpaces, int availableSpaces, List<string> vehicles)
+        {
+            ParkingID = id;
+            Location = location;
+            TotalSpaces = totalSpaces;
+            AvailableSpaces = availableSpaces;
+            Vehicles = vehicles ?? new List<string>();
+        }
+
+    }
+}
